6c40e53d304318b29ad4d62eddd8ebf45c83b562
abdullahshaker10 <abdullah.shaker2017@gmail.com>
2020-10-18 15:05:21 +0200

    th

--- store/views.py
+++ store/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, ListView, UpdateView, View, CreateView, FormView
-from .models import *
+from django.views.generic import TemplateView, ListView, View, CreateView, FormView
+from .models import Order, Product, OrderItem, ShippingAddress
 import pdb
 import datetime
 import json
